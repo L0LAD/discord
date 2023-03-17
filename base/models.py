ce6f339077a8095dedf125744b37c19e58b3a14a
L0LAD <audrey.denoual@live.fr>
2023-03-17 23:28:32 +0100

    Fil d'actualité

--- base/models.py
+++ base/models.py
@@ -18,9 +18,6 @@ class Room(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        ordering = ['-updated', '-created']
-
     def __str__(self):
         return self.name
 
@@ -30,6 +27,9 @@ class Message(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.body[0:50]
