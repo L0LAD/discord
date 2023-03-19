739763af6b9d25fa1d4226baf04c91a48884d310
L0LAD <audrey.denoual@live.fr>
2023-03-19 23:03:04 +0100

    Zoom images

--- base/models.py
+++ base/models.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(unique=True, max_length=200, null=True)
+    username = models.CharField(unique=True, max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
 
     avatar = models.ImageField(null=True, default="avatar.svg")
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
 
 class ImageModel(models.Model):
     image = models.ImageField(default=None, null=True, blank=True)
